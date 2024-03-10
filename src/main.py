2634ba07ce79187b636625c2538768ed1a1328bf
Markelich <87765916+Markelich@users.noreply.github.com>
2024-03-10 16:09:12 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,14 +18,16 @@ def get_users_list(request: Request):
     list_of_users = df.groupby('DstIP').sum()[['Pkts', 'Octets']].sort_values(by='Octets', ascending=False)  # список пользователей c + общий pkts и oktets для каждого
     list_of_users['Octets'] = list_of_users['Octets'].apply(lambda x: round(x, 2))
     list_of_users = list_of_users.reset_index().to_dict('records')
-    # return list_of_users
-    return templates.TemplateResponse("base.html", {"request": request, "list_of_users": list_of_users})
+    return list_of_users
+    # return templates.TemplateResponse("base.html", {"request": request, "list_of_users": list_of_users})
 
 
 @router.get("/{user_addres}")
 def get_users_list(user_addres):
-    user_info = df.to_dict('records')
-    return [user for user in user_info if user.get("DstIP") == user_addres]
+    list_of_serveses = df.groupby(['SrcIP', 'DstIP']).sum()[['Pkts', 'Octets']].sort_values(by='Octets', ascending=False)   # список сервисов обрщаемых конкретным пользователей + общий pkts и oktets для каждого
+    list_of_serveses['Octets'] = list_of_serveses['Octets'].apply(lambda x: round(x, 2))
+    list_of_serveses = list_of_serveses.reset_index().to_dict('records')
+    return [user for user in list_of_serveses if user.get("DstIP") == user_addres]
 
 
 
